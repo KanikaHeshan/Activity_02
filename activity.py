147e92c08d45638403c0c8c54f26f6a103a1856c
Heshan <kanikaheshan7@gmail.com>
2021-10-07 00:13:04 +0530

    Modified the python file

---
+++ activity.py
@@ -0,0 +1,12 @@
+print ("Temperture Converter")
+
+temp=float(input("Enter the Temperature:"))
+sing=input("Input 'F' for Fahrenheit or 'C' for Celsius:")
+
+if sing == "F":
+    temp = ((temp-32.00)*5.00/9.00)
+    print ("Temperature in Celsius: ",temp, "C")
+    
+elif sing == "C":
+    temp = ((temp*9.00/5.00)+32.00)
+    print ("Temperature in Fahrenheit: ", temp, "F")
